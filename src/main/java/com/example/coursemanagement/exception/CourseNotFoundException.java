50d70474b5ece949d786b6e951f5d82f1270bc16
SarahMubarak643 <sarnjssjah@gmail.com>
2026-07-18 13:27:02 +0300

    update
    update

--- src/main/java/com/example/coursemanagement/exception/CourseNotFoundException.java
+++ src/main/java/com/example/coursemanagement/exception/CourseNotFoundException.java
@@ -1,8 +1,6 @@
 package com.example.coursemanagement.exception;
 
-/**
- * Thrown when a Course cannot be found by its id.
- */
+
 public class CourseNotFoundException extends RuntimeException {
 
     public CourseNotFoundException(String message) {
